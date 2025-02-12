297e5eddd2b8a1722d416f8aee4227396d3fd94b
naes4170 <shh044@ucsd.edu>
2025-02-11 18:42:13 -0800

    My version of 5. Feedback (It's not optimal so I will learn and adjust from the answer key))

--- python/stroop-experiment/stroop.py
+++ python/stroop-experiment/stroop.py
@@ -12,10 +12,13 @@ placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineCol
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 fixation_cross = visual.TextStim(win,text="+", height=15, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200], autoDraw=True)
+Feedback = visual.TextStim(win,text="Incorrect", height=15, color="black")
 RTs = open("RTs.csv", mode="w", newline="") #I tried what Chatgpt suggested but it didn't work, so I did it on my own through incorporating your in-class code!
-
+key_pressed=False #Saw this in the answer key, put here in case
 while True:
     cur_stim = random.choice(stimuli)
+    Ans = list(cur_stim[0])
+    print(cur_stim[0])
     word_stim.setText(cur_stim)
     word_stim.setColor(cur_stim)
     fixation_cross.draw()
@@ -28,7 +31,12 @@ while True:
     win.flip()
     timer = core.Clock()
     key_pressed = event.waitKeys(keyList=['r','o','y','g','b','q'])
+    print(key_pressed)
     placeholder.draw()
+    if key_pressed != Ans:
+        Feedback.draw()
+        core.wait(1)
+        print(cur_stim)    
     win.flip()
     dur = timer.getTime()
     print(dur)
