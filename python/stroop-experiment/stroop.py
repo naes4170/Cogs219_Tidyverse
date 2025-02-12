2d0173b8c3632df3cb12bb85959c4b59898458d3
naes4170 <shh044@ucsd.edu>
2025-02-11 22:38:24 -0800

    I made a bunch of edits after checking out the answer key, mainly to optimize things. My original code doesn't work for the too_slow note, I think it's because I placed it wrongfully.

--- python/stroop-experiment/stroop.py
+++ python/stroop-experiment/stroop.py
@@ -13,8 +13,10 @@ word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 fixation_cross = visual.TextStim(win,text="+", height=15, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200], autoDraw=True)
 Feedback = visual.TextStim(win,text="Incorrect", height=15, color="black")
+Too_slow = visual.TextStim(win,text="Too slow", height=15, color="black")
 RTs = open("RTs.csv", mode="w", newline="") #I tried what Chatgpt suggested but it didn't work, so I did it on my own through incorporating your in-class code!
 key_pressed=False #Saw this in the answer key, put here in case
+timer = core.Clock()
 while True:
     cur_stim = random.choice(stimuli)
     Ans = list(cur_stim[0])
@@ -29,17 +31,23 @@ while True:
     placeholder.draw()
     word_stim.draw()
     win.flip()
-    timer = core.Clock()
-    key_pressed = event.waitKeys(keyList=['r','o','y','g','b','q'])
-    print(key_pressed)
-    placeholder.draw()
-    if key_pressed != Ans:
-        Feedback.draw()
-        core.wait(1)
-        print(cur_stim)    
-    win.flip()
+    timer.reset()
     dur = timer.getTime()
+    key_pressed = event.waitKeys(keyList=['r','o','y','g','b','q'], maxWait=2)
+    if not key_pressed:
+        Too_slow.draw()
+        win.flip()
+        core.wait(1)
+    elif key_pressed == Ans:
+        pass
+    elif key_pressed == ['q']:
+        break
+    else:
+        Feedback.draw()
+        win.flip()
+        core.wait(1)
     print(dur)
+
     RTs.write(str(dur)+'\n')
     timer.reset()
 
